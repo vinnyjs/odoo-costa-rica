b89346acb924b33de59db4d9e5bee621417d1d2b
Fabio Vilchez <fabio.vilchez@clearcorp.co.cr>
2016-04-08 11:39:14 -0600

    [ADD] - l10n_cr_currency_rate_update: Added attribute hours to selection currency update frequency.

--- l10n_cr_currency_rate_update/models/currency_rate_update_service.py
+++ l10n_cr_currency_rate_update/models/currency_rate_update_service.py
@@ -2,13 +2,20 @@
 # © 2016 ClearCorp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models
+from openerp import models, fields
 from openerp.tools.translate import _
 
 
 class CurrencyRateUpdateService(models.Model):
 
     _inherit = 'currency.rate.update.service'
+
+    interval_type = fields.Selection(
+        selection_add=[('hours', 'Hour(s)'),
+                       ('days', 'Day(s)'),
+                       ('weeks', 'Week(s)'),
+                       ('months', 'Month(s)')],
+        string='Currency update frequency', default='days')
 
     _sql_constraints = [(
         'curr_service_unique',
