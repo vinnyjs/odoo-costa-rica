7d4ec88555458d345dfd13040d76dc7c528f9eff
vinitoreto@gmail.com <vinitoreto@gmail.com>
2017-07-28 20:06:09 -0600

    Se mueven archivos a carpeta TODO para ser migrados

--- l10n_cr_account_invoice_D-151/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# © <YEAR(S)> ClearCorp
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-import l10n_cr_account_invoice_line
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- l10n_cr_hr_holidays/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 ClearCorp
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-import l10n_cr_hr_holidays
